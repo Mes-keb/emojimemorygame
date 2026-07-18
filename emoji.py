7bd83e21993de4fdc623386956fb5c2a22b2ebcd
Meskeb <omeskebi15@gmail.com>
2026-07-18 11:35:52 -0700

    second

---
+++ emoji.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+import random
+
+difficulty_levels = {
+    "Easy": ["🐶", "😸", "🐭", "🐹"],
+    "Medium": [
+        "🐶", "😸", "🐭", "🐹",
+        "🦊", "🐻", "🐼", "🐸"
+    ], 
+    "Hard": [
+        "🐶", "😸", "🐭", "🐹",
+        "🦊", "🐻", "🐼", "🐸"
+        "🐵", "🦁", "🐯", "🐨"
+    ]
+}
+
+
+Class EmojiMemoryGame:
